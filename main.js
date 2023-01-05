939cf36dce3ef2e9119c4fd9bb20b608d91bbb1f
ItsRosemary79 <itsrosemary79@gmail.com>
2023-01-05 01:24:55 -0800

    completed header section

---
+++ main.js
@@ -0,0 +1,4 @@
+// let searchForm = document.querySelector('.search-form') 
+document.querySelector('#search-btn').onclick = () =>{
+    document.querySelector('.search-form').classList.toggle('active')
+}
