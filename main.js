dacd3088d99435b48eaeef9d36bfb6b9a510ab77
ItsRosemary79 <itsrosemary79@gmail.com>
2023-02-23 21:39:34 -0800

    completed section

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // let searchForm = document.querySelector('.search-form') 
 document.querySelector('#search-btn').onclick = () =>{
     document.querySelector('.search-form').classList.toggle('active')
-}
+} 
+
+let shopppingCart = document.querySelector('.shopping-cart') 
+document.querySelector('#cart-btn').onclick = () =>{
+    shopppingCart.classList.toggle('active')
+} 
